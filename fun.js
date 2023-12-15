b5da0b2a689247d0110ded20c6a6e0d3b6d51e52
Farhan Ali <farhanyounas5204@gmail.com>
2023-12-15 21:32:55 +0500

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,15 @@
+"use strict";
+// functions
+function add(num1 = 10, num2 = 10) {
+    let result = num1 + num2;
+    return result;
+}
+const values = add();
+console.log(values);
+function song(Song, singer) {
+    console.log(Song);
+    if (singer) {
+        console.log("heheherhehhhhhhhe");
+    }
+}
+song("leo", "Farhan");
